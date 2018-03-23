5c8e2de4ca6a27ee6655d2e4483d009b77e1dbb1
huixiaoyang <37188419+huixiaoyang@users.noreply.github.com>
2018-03-24 02:01:10 +0800

    Update MainActivity.java

--- app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
+++ app/src/main/java/mg/studio/weatherappdesign/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +19,13 @@ import java.net.URL;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import org.json.*;
+import java.lang.String;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,27 +35,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void btnClick(View view) {
-        new DownloadUpdate().execute();
+    public  void onReceive(Context context) {
+        ConnectivityManager connectivityManager=(ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo  mobNetInfo=connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        NetworkInfo  wifiNetInfo=connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-        SimpleDateFormat formatter_date = new SimpleDateFormat("MM/dd/yyyy ");
-        Date curDate = new Date(System.currentTimeMillis());
-        String str= formatter_date.format(curDate);
-        ((TextView) findViewById(R.id.tv_date)).setText(str);
+        if (!mobNetInfo.isConnected() && !wifiNetInfo.isConnected()) {
+            Toast.makeText(getApplicationContext(), "please check your Internet", Toast.LENGTH_LONG).show();
+        }else {
+            new DownloadUpdate().execute();
 
-        SimpleDateFormat formatter_week = new SimpleDateFormat("EEEE");
-        Date curWeek = new Date(System.currentTimeMillis());
-        String str2= formatter_week.format(curWeek);
-        ((TextView) findViewById(R.id.curweekday)).setText(str2);
+            SimpleDateFormat formatter_date = new SimpleDateFormat("MM/dd/yyyy ");
+            Date curDate = new Date(System.currentTimeMillis());
+            String str= formatter_date.format(curDate);
+            ((TextView) findViewById(R.id.tv_date)).setText(str);
+
+            SimpleDateFormat formatter_week = new SimpleDateFormat("EEEE");
+            Date curWeek = new Date(System.currentTimeMillis());
+            String str2= formatter_week.format(curWeek);
+            ((TextView) findViewById(R.id.curweekday)).setText(str2);
+            ((TextView) findViewById(R.id.tv_location)).setText("chongqing");
+        }
     }
 
-
+    public void btnClick(View view) {
+        Context context = getApplicationContext();
+        onReceive(context);
+    }
+    
     private class DownloadUpdate extends AsyncTask<String, Void, String> {
-
-
         @Override
         protected String doInBackground(String... strings) {
-            String stringUrl = "http://mpianatra.com/Courses/info.txt";
+            //String stringUrl = "http://mpianatra.com/Courses/info.txt";
+            String stringUrl ="https://api.seniverse.com/v3/weather/now.json?key=snjr8qqdaam5soy1&location=chongqing&language=zh-Hans&unit=c";
             HttpURLConnection urlConnection = null;
             BufferedReader reader;
 
@@ -78,9 +99,16 @@ public class MainActivity extends AppCompatActivity {
                     // Stream was empty.  No point in parsing.
                     return null;
                 }
-                //The temperature
-                return buffer.toString();
-
+                String  strWeatherData=buffer.toString();
+                try {
+                    JSONObject jsonWeatherData = new JSONObject(strWeatherData);
+                    String Temperature=jsonWeatherData.getJSONArray("results").getJSONObject(0).getJSONObject("now").getString("temperature");
+                    //The temperature
+                    return Temperature;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                //return buffer.toString();
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             } catch (ProtocolException e) {
@@ -88,7 +116,6 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             return null;
         }
 
